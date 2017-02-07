c8bdcc8493d2e4997ec007e9ce75bdc861c1382d
MrtMerlin <mrtmerlin@gmail.com>
2017-02-07 13:22:27 +0000

    Updated_JSON
    using aJax and methods to call information.

--- cds/js/fetch.js
+++ cds/js/fetch.js
@@ -1,33 +1,41 @@
+// global variables
+var JSONData;
+var responseComplete = false;
+
+// jQuery Api 
 $(document).ready(function(){
-    
-    /*$.getJSON("http://api.computing-moodle.co.uk/api.php/display/breaks/", function(breaks){
-    });*/
+
+
+    getData('http://api.computing-moodle.co.uk/api.php/display/urls/');
+    var refreshId = setInterval(function(){
+        if(responseComplete){
+          $('#toptext').html(JSONData.urls[0].toptext);  
+        } 
+    },10000);
+});
+
+function getData(theUrl) {
+    responseComplete = false;
 
     $.ajax({
-        url: 'http://api.computing-moodle.co.uk/api.php/display/urls/',
+        url: theUrl,
         dataType: 'json',
         type: 'get',
         cache: false,
         success: function(data) {
             $(data.urls).each(function(index, value) {
-                //if (value.startweek < 1) {
-                    $("div").each(function() {
-                        $(this).append("<li>" + value.startweek + "&nbsp&nbsp&nbsp&nbsp&nbsp" + value.endweek + "&nbsp&nbsp&nbsp&nbsp&nbsp" + value.url + "&nbsp&nbsp&nbsp&nbsp&nbsp" + value.toptext + + "&nbsp&nbsp&nbsp&nbsp&nbsp" + value.bottomtext + "&nbsp&nbsp&nbsp&nbsp&nbsp" + "</li>");
-                    });
-                //}
+                $('#breaks').each(function() {
+                    $(this).append("<li>" + value.startweek + "&nbsp&nbsp&nbsp&nbsp&nbsp" + value.endweek + "&nbsp&nbsp&nbsp&nbsp&nbsp" + value.url + "&nbsp&nbsp&nbsp&nbsp&nbsp" + value.toptext  + "&nbsp&nbsp&nbsp&nbsp&nbsp" + value.bottomtext + "&nbsp&nbsp&nbsp&nbsp&nbsp" + "</li>");
+                });
+                storeData(data);
             });
-        }
+            
+            
+        }, 
     });
-});
+}
 
-
-
-
-/*
-
-$("div").append('<p>' + result['breaks'][0]['idbreaks'] + "&nbsp&nbsp&nbsp&nbsp&nbsp" + result['breaks'][0]['startweek'] 
-            + "&nbsp&nbsp&nbsp&nbsp&nbsp" + result['breaks'][0]['endweek'] + "&nbsp&nbsp&nbsp&nbsp&nbsp" + result['breaks'][0]['startdate'] 
-            + "&nbsp&nbsp&nbsp&nbsp&nbsp" + result['breaks'][0]['enddate'] + "&nbsp&nbsp&nbsp&nbsp&nbsp" + result['breaks'][0]['description'] 
-            + '</p>' + '<br>');
-
-*/
+function storeData(data){
+     JSONData = data;
+     responseComplete = true;
+}
