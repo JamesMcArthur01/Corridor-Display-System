fcca660dd9af86879816a2b4e53e8c469015b7fa
MrtMerlin <mrtmerlin@gmail.com>
2017-02-10 14:12:21 +0000

    DateFunc, currentDate add no compare.
    DateFunc, currentDate add no compare yet, currently working on this for comparison on from the firstWeek compare to currentDate. Backbone.

--- cds/js/fetch.js
+++ cds/js/fetch.js
@@ -1,40 +1,80 @@
+// use strict
+"use strict";
+
 // global variables
 var JSONData;
+var now;
+var firstWeek;
 var responseComplete = false;
 
-// jQuery Api 
+// jQuery Api
 $(document).ready(function(){
 
+    // calling method getdata
+	getData('http://api.computing-moodle.co.uk/api.php/display/urls/');
+  getData('http://api.computing-moodle.co.uk/api.php/display/breaks/');
+  getData('http://api.computing-moodle.co.uk/api.php/display/messages/');
+  getData('http://api.computing-moodle.co.uk/api.php/display/lessons/');
+  getData('http://api.computing-moodle.co.uk/api.php/display/start/');
+    // interval set to screen.
 
-    getData('http://api.computing-moodle.co.uk/api.php/display/urls/');
     var refreshId = setInterval(function(){
         if(responseComplete){
-          $('#toptext').html(JSONData.urls[0].toptext);  
-        } 
+          $('#toptext').html(JSONData.urls[0].toptext);
+        }
     },10000);
 });
 
+// getData method, will get data from the URL populate page.
 function getData(theUrl) {
     responseComplete = false;
 
+    // data collection.
     $.ajax({
         url: theUrl,
         dataType: 'json',
         type: 'get',
         cache: false,
         success: function(data) {
-            $(data.urls).each(function(index, value) {
-                $('#breaks').each(function() {
-                    $(this).append("<li>" + value.startweek + "&nbsp&nbsp&nbsp&nbsp&nbsp" + value.endweek + "&nbsp&nbsp&nbsp&nbsp&nbsp" + value.url + "&nbsp&nbsp&nbsp&nbsp&nbsp" + value.toptext  + "&nbsp&nbsp&nbsp&nbsp&nbsp" + value.bottomtext + "&nbsp&nbsp&nbsp&nbsp&nbsp" + "</li>");
-                });
-                storeData(data);
-            });
-            
-            
-        }, 
+    			$(data.urls).each(function(index, value) {
+    				storeData(data);
+    			});
+        },
+    });
+    $.ajax({
+        url: theUrl,
+        dataType: 'json',
+        type: 'get',
+        cache: false,
+        success: function(sDate) {
+    			$(sDate.start).each(function(index, value) {
+    				dateFunc(sDate);
+    			});
+        },
     });
 }
+// date information
+function dateFunc(sDate) {
+  firstWeek = sDate;
+  responseComplete = true;
+}
 
+function currentDate() {
+	now = new Date();
+}
+// compare date
+// function compareDate (firstWeek) {
+//   var currentDate = Date();
+//   var termDate = firstWeek;
+//
+//   if(currentDate < termDate) {
+//     alert("currentDate is greater than firstWeek");
+//   }
+//   else {
+//     alert("firstWeek is greater than currentDate");
+//   }
+// }
+// storeData method to hold data.
 function storeData(data){
      JSONData = data;
      responseComplete = true;
