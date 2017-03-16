be14bd4133d457b1ca25bd374c105ac3ae6d631e
kLukask <lukas1033@gmail.com>
2017-03-16 15:12:33 +0000

    Add files via upload

---
+++ Image.js
@@ -0,0 +1,13 @@
+
+
+
+$(document).ready(function(){
+jQuery('#url').qrcode({
+text : "http://www.lincolncollege.ac.uk/"
+      });
+});
+
+
+setInterval(function(){
+  $("#toptext").toggle();
+},3000);
